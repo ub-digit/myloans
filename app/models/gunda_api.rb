1ab2f31960450edecfed70ce19da919b07f36f09
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-03-25 12:31:19 +0100

    Add special handling when only one request exists due to inconsistencies in SOAP API:

--- app/models/gunda_api.rb
+++ app/models/gunda_api.rb
@@ -12,7 +12,6 @@ class GundaApi
 
     response = response.body[:patron_account_response]
     #pp response
-    
 
     user.name = response[:patron_name][:last]
     user.expiration_date = response[:expiration_date]
@@ -32,35 +31,59 @@ class GundaApi
 
     # Create loan objects
     loans = []
-    response[:checkouts][:checkout].each do |checkout|
-      loan = Loan.new
+    if response[:checkouts]
+      response[:checkouts][:checkout].each do |checkout|
+        loan = Loan.new
 
-      loan.barcode = checkout[:checkout_item][:@barcode]
-      loan.title = checkout[:checkout_bibliographic_record][:@title]
-      loan.due_date = checkout[:@due_date]
-      loan.recallable_date = checkout[:@recallable_date]
-      loan.status = checkout[:@status]
-      loan.renewable = checkout[:@renewable]
+        loan.barcode = checkout[:checkout_item][:@barcode]
+        loan.title = checkout[:checkout_bibliographic_record][:@title]
+        loan.due_date = checkout[:@due_date]
+        loan.recallable_date = checkout[:@recallable_date]
+        loan.status = checkout[:@status]
+        loan.renewable = checkout[:@renewable]
 
-      loans << loan
+        loans << loan
+      end
     end
     user.current_loans = loans
 
     # Create request objects
     requests = []
-    response[:requests][:request].each do |req|
-      request = Request.new
-      
-      request.title = req[:request_bibliographic_record][:@title]
-      request.pickup_location = req[:pickup_location][:@pickup_location_name]
-      request.expiration_date = req[:@expiration_date]
-      request.status = req[:@status]
-      request.queue_position = req[:@queue_position]
+    if response[:requests]
 
-      requests << request
+      # Special handling if only one record exists
+      if response[:requests][:request].first.first == :request_bibliographic_record   
+        puts response[:requests][:request]
+        req = response[:requests][:request]
+        request = Request.new
+            #puts req
+            request.title = req[:request_bibliographic_record][:@title]
+            request.pickup_location = req[:pickup_location][:@pickup_location_name]
+            request.expiration_date = req[:@expiration_date]
+            request.status = req[:@status]
+            request.queue_position = req[:@queue_position]
+
+            requests << request
+      
+      else
+
+        if response[:requests][:request]
+          response[:requests][:request].each do |req|
+            request = Request.new
+            #puts req
+            request.title = req[:request_bibliographic_record][:@title]
+            request.pickup_location = req[:pickup_location][:@pickup_location_name]
+            request.expiration_date = req[:@expiration_date]
+            request.status = req[:@status]
+            request.queue_position = req[:@queue_position]
+
+            requests << request
+          end
+        end
+      end
     end
     user.requests = requests
-    
+
     return user
   end
 
