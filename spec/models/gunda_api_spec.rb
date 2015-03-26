ba7dce097475f74b3042740369139c236594784b
Per Larsson <per.larsson@ub.gu.se>
2015-03-26 16:44:03 +0100

    Added more tests.

--- spec/models/gunda_api_spec.rb
+++ spec/models/gunda_api_spec.rb
@@ -23,6 +23,22 @@ RSpec.describe GundaApi, :type => :model do
       expect(user).to be_truthy
     end
 
+    context "for a user with one request" do
+      it "will return a user with a list of one request" do
+        message = { barcode: "7" }
+        fixture = File.read("spec/fixtures/patronAccount/one_request.xml")
+        savon.expects(:patron_account).with(message: message).returns(fixture)
+
+        user = GundaApi.find_user("7")
+        expect(user).to be_truthy
+        expect(user.requests).to be_truthy
+        expect(user.requests.first).to be_truthy
+        expect(user.requests.class == Array).to be_truthy
+        expect(user.requests.length == 1).to be_truthy
+        expect(user.requests.first.class == Request).to be_truthy
+      end
+    end
+
     context "for a user with many requests" do
       it "will return a user with a list of requests" do
         message = { barcode: "6" }
@@ -32,10 +48,58 @@ RSpec.describe GundaApi, :type => :model do
         user = GundaApi.find_user("6")
         expect(user).to be_truthy
         expect(user.requests).to be_truthy
-        expect(user.requests.first).to be_truthy
+        expect(user.requests.class == Array).to be_truthy
+        expect(user.requests.length >= 2).to be_truthy
         expect(user.requests.each{|req| req.class == Request}).to be_truthy
       end
     end
-  end
 
+
+    context "for a user with one loan" do
+      it "will return a user with a list of one loan" do
+        message = { barcode: "8" }
+        fixture = File.read("spec/fixtures/patronAccount/one_checkout.xml")
+        savon.expects(:patron_account).with(message: message).returns(fixture)
+
+        user = GundaApi.find_user("8")
+        expect(user).to be_truthy
+        expect(user.current_loans).to be_truthy
+        expect(user.current_loans.first).to be_truthy
+        expect(user.current_loans.class == Array).to be_truthy
+        expect(user.current_loans.length == 1).to be_truthy
+        expect(user.current_loans.first.class == Loan).to be_truthy
+      end
+    end
+
+    context "for a user with many loans" do
+      it "will return a user with a list of loans" do
+        message = { barcode: "10" }
+        fixture = File.read("spec/fixtures/patronAccount/many_checkouts.xml")
+        savon.expects(:patron_account).with(message: message).returns(fixture)
+
+        user = GundaApi.find_user("10")
+        expect(user).to be_truthy
+        expect(user.current_loans).to be_truthy
+        expect(user.current_loans.class == Array).to be_truthy
+        expect(user.current_loans.length >= 2).to be_truthy
+        expect(user.current_loans.each{|loan| loan.class == Loan}).to be_truthy
+      end
+    end
+
+    context "for a user with many fines" do
+      it "will return a user with a list of fines" do
+        message = { barcode: "14" }
+        fixture = File.read("spec/fixtures/patronAccount/two_fines.xml")
+        savon.expects(:patron_account).with(message: message).returns(fixture)
+
+        user = GundaApi.find_user("14")
+        expect(user).to be_truthy
+        expect(user.fines).to be_truthy
+        expect(user.fines.class == Array).to be_truthy
+        expect(user.fines.length >= 2).to be_truthy
+        expect(user.fines.each{|fine| fine.class == Fine}).to be_truthy
+      end
+    end
+
+  end
 end
